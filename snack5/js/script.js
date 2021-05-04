621af393bdfaaa203873c0d0060c578363d4b62d
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-04 18:03:30 +0200

    Final Refactoring

--- snack5/js/script.js
+++ snack5/js/script.js
@@ -14,8 +14,10 @@ var list = document.getElementById("list");
 // CHIEDI 6 VOLTE ALL'UTENTE UN NUMERO E SE E' DISPARI INSERISCILO NELLA LISTA
 for (i = 1; i <= 6; i++) {
 
+    // CHIEDI IL NUMERO ALL'UTENTE
     userNumber = parseInt(prompt("Enter a number, please."));
 
+    // SE L'UTENTE INSERISCE EFFETTIVAMENTE UN NUMERO, ALLORA VERIFICA CHE SIA DISPARI E NEL CASO INSERISCILO NELL'ARRAY
     if (!isNaN(userNumber)) {
 
         numberOdd = oddCalcolator (userNumber);
@@ -28,5 +30,5 @@ for (i = 1; i <= 6; i++) {
     
 }
 
+// STAMPA L'ARRAY SULL'HTML
 list.innerHTML = numberList;
-console.log(numberList);
