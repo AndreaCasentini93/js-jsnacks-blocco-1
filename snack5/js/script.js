9fc835bd50a2b8a575674e6926e5333549b07efc
AndreaCasentini93 <casentini.andrea1993@gmail.com>
2021-05-04 14:55:43 +0200

    Function Complete

--- snack5/js/script.js
+++ snack5/js/script.js
@@ -1,7 +1,30 @@
-/*
-Crea un array vuoto.
-Chiedi per 6 volte all’utente di inserire un numero,
-se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
-Stampa l'array nella console.
-*/
+// FUNZIONE PER VALUTARE SE UN NUMERO E' DISPARI
+function oddCalcolator (number) {
 
+    return number % 2 == 1
+
+}
+
+// VARIABILI GLOBALI
+var numberList = [];
+var userNumber;
+var numberOdd;
+
+// CHIEDI 6 VOLTE ALL'UTENTE UN NUMERO E SE E' DISPARI INSERISCILO NELLA LISTA
+for (i = 1; i <= 6; i++) {
+
+    userNumber = parseInt(prompt("Enter a number, please."));
+
+    if (!isNaN(userNumber)) {
+
+        numberOdd = oddCalcolator (userNumber);
+
+        if (numberOdd) {
+            numberList.push(userNumber);
+        }
+
+    }
+    
+}
+
+console.log(numberList);
